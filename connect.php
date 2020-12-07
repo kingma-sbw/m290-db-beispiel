49971cde3ff647a4486a839f6f1f59176cc8ad19
jkingma <kingma@sbw-media.ch>
2020-12-07 14:14:00 +0100

    DB Connection

--- connect.php
+++ connect.php
@@ -6,5 +6,5 @@ $lang = "de";
 if( strstr($_SERVER['SERVER_NAME'], 'local') ) {
     $db = mysqli_connect('localhost','m290','m290','m290_links') or die('datenbank verbindung nicht möglich');
 } else {
-    $db = mysqli_connect('localhost','jkingma_m290','m290-db','jkingma_m290') or die('datenbank verbindung nicht möglich');
+    $db = mysqli_connect('localhost','m290','m290-db','jkingma_m290') or die('datenbank verbindung nicht möglich');
 }
